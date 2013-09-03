b17c0202079ec3e623f4886b6ba84ad48ce40c38
bmatic <bojan@lis.rs>
2013-09-03 15:12:36 +0200

    [fix] Move views from model directory

---
+++ makler/views/institutions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+
+from .model.institution import Institution
+from .model.session import Session
+
+
+@view_config(route_name='institution_create',
+             request_method='POST')
+def institution_create(request):
+
+    data = dict(request.params)
+    institution = Institution(**data)
+    Session.add(institution)
+    Session.flush()
+    Session.commit()
+
+    message = "Uspešno ste dodali instituciju."
+    request.session.flash(message)
+
+    return HTTPFound(location=request.route_path('home'))
---
+++ makler/views/instruments.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+
+from .model.instrument import Instrument
+from .model.session import Session
+
+
+@view_config(route_name='instrument_create',
+             request_method='POST')
+def instrument_create(request):
+    data = dict(request.params)
+    instrument = Instrument(**data)
+    Session.add(instrument)
+    Session.flush()
+    Session.commit()
+
+    message = "Uspešno ste dodali aparat."
+    request.session.flash(message)
+
+    return HTTPFound(location=request.route_path('home'))
