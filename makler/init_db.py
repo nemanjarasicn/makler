eda62b35cdebf95ee6db99aed66fd31084528b4a
bmatic <bojan@lis.rs>
2013-09-03 13:48:02 +0200

    Add filename parameter to init_db script

--- makler/init_db.py
+++ makler/init_db.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import os
 from sqlalchemy import create_engine
 
 
-engine = create_engine(
-    'sqlite:////home/bojan/dev/maklerdb/makler.sqlite', echo=True)
+def init_db(db_file):
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    fullpath_db = os.path.abspath(
+        os.path.join(cwd, db_file))
 
-from model.base import meta
-from model import schema  # NOQA
-meta.create_all(bind=engine)
+    engine = create_engine(
+        'sqlite:///' + fullpath_db, echo=True)
+
+    from model.base import meta
+    from model import schema  # NOQA
+    meta.create_all(bind=engine)
+
+
+if __name__ == '__main__':
+    init_db(sys.argv[1]) or sys.exit(1)
