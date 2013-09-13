769476df28de95d87517340158792969592767f6
bmatic <bojan@lis.rs>
2013-09-13 13:07:24 +0200

    Version bump to 0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = [
     ]
 
 setup(name='makler',
-      version='0.1.2',
+      version='0.2',
       description='makler',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
