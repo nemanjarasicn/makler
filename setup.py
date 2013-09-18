48c84b6b44eb9ca0796bf677d27319f376a3be62
bmatic <bojan@lis.rs>
2013-09-18 16:55:27 +0200

    Version bump to 0.3.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = [
     ]
 
 setup(name='makler',
-      version='0.2.3',
+      version='0.3.0',
       description='makler',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
